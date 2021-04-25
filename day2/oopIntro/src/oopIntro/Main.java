5c4f830f02ab00e856c42f660f7ffbd02438822b
orhanari <orhanari55@gmail.com>
2021-04-25 15:01:37 +0300

    Update Main.java

--- day2/oopIntro/src/oopIntro/Main.java
+++ day2/oopIntro/src/oopIntro/Main.java
@@ -4,19 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// Çıktýda iki kez "Ben çalýţtým" yazmasýnýn sebebi, iki kere Product() constructor çađýrdýđýmýz içindir.
-		// Ýlk satýr ise içerisinde deđiţkenler olan constructor ile bađýntýlýdýr.
-		// Yani ayný adda iki constructor tanýmlanabilir ve iţlem durumlarýna göre belirli constructor çađýrýlýr.
+		// Çıktıda iki kez "Ben çalıştım" yazmasının sebebi, iki kere Product() constructor çağırdığımız içindir.
+		// İlk satır ise içerisinde değişkenler olan constructor ile bağıntılıdır.
+		// Yani aynı adda iki constructor tanımlanabilir ve işlem durumlarına göre belirli constructor çağırılır.
 		
 		Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB RAM");
 		
-		Product product2 = new Product(); //Referans oluţturma, instance
+		Product product2 = new Product(); //Referans oluşturma, instance
 		product2.id = 2;
 		product2.name="Lenovo V15";
 		product2.unitPrice = 16000;
 		product2.detail = "32 GB RAM";
 		
-		Product product3 = new Product(); //Referans oluţturma, instance
+		Product product3 = new Product(); //Referans oluşturma, instance
 		product3.id = 3;
 		product3.name="HP 5";
 		product3.unitPrice = 10000;
@@ -31,7 +31,7 @@ public class Main {
 			System.out.println(product.name);
 		}
 		
-		System.out.println("Dizideki eleman sayýlarý: " + products.length);
+		System.out.println("Dizideki eleman sayıları: " + products.length);
 		
 		
 		Category category1 = new Category();
