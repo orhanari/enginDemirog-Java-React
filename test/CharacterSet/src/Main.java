700cc47b370d5464af70fc22fb60b0c3834a10b7
orhan <orhan@DESKTOP-O2M6P1A>
2021-05-03 11:22:49 +0300

    Test

---
+++ test/CharacterSet/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Yorum satırı türkçe karakterler test İÇŞÖÜĞ
+		System.out.println("Türkçe karakterler test ÜĞİŞÇÖ");
+	}
+
+}
