8ff8aab0e78b6da6321ebb22fec26b43ccd17979
orhanari <orhanari55@gmail.com>
2021-05-03 11:21:24 +0300

    Delete testrepo directory

--- testrepo/CharacterSet/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		// Yorum satırı türkçe karakterler test içöüğ
-		System.out.println("Türkçe karakterler test İŞÇÖÜĞ");
-	}
-
-}
--- testrepo/CharacterSet2/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		// Türkçe karakterler test İŞÇÖÜĞ
-		System.out.println("Türkçe karakterler test İŞÇÖĞÜ");
-	}
-
-}
