4f91794a8b780d4eb48635a07313888cb84a2004
orhanari <orhanari55@gmail.com>
2021-05-03 14:32:39 +0300

    Delete homeworks/homeworkDay3 directory

--- homeworks/homeworkDay3/src/homeworkDay3/Instructor.java
+++
@@ -1,7 +0,0 @@
-package homeworkDay3;
-
-public class Instructor extends User{
-	//Instructor > Eðitmen
-	int instructorId;
-	String companyName;
-}
--- homeworks/homeworkDay3/src/homeworkDay3/User.java
+++
@@ -1,9 +0,0 @@
-package homeworkDay3;
-
-public class User {
-	//Ortak, extend edilecek yer.
-	int id;
-	String name;
-	String mail;
-	String phone;
-}
