4599ef1e9d649b99ac5dd604de4c3ba161e4e6ae
orhanari <orhanari55@gmail.com>
2021-05-06 10:34:41 +0300

    Add files via upload
    In this course, the topic of interfaces is completed.

---
+++ day4/interfaces/src/interfaces/CustomerManager.java
@@ -0,0 +1,45 @@
+﻿package interfaces;
+
+public class CustomerManager {
+	
+	/*private Logger logger;
+	
+	public CustomerManager(Logger logger) {
+		this.logger = logger;
+	}*/
+	
+	private Logger[] loggers;
+	
+	public CustomerManager(Logger[] loggers) {
+		this.loggers = loggers;
+	}
+	// loosly - tightly coupled
+	public void add(Customer customer) {
+		System.out.println("Müşteri eklendi: " + customer.getFirstName());
+		
+		/*DatabaseLogger logger = new DatabaseLogger();
+		logger.log(customer.getFirstName() + " veritabanına loglandı");*/
+		
+		//this.logger.log(customer.getFirstName());
+		/*for(Logger logger : loggers) {
+			logger.log(customer.getFirstName());
+		}*/
+		
+		
+		Utils.runLoggers(loggers, customer.getFirstName());
+		
+	}
+	public void delete(Customer customer) {
+		System.out.println("Müşteri silindi: " + customer.getFirstName());
+		
+		/*DatabaseLogger logger = new DatabaseLogger();
+		logger.log(customer.getFirstName() + " veritabanına loglandı");*/
+		
+		//this.logger.log(customer.getFirstName());
+		/*for(Logger logger : loggers) {
+			logger.log(customer.getFirstName());
+		}*/
+		
+		Utils.runLoggers(loggers, customer.getLastName());
+	}
+}
---
+++ day4/interfaces/src/interfaces/Logger.java
@@ -0,0 +1,5 @@
+﻿package interfaces;
+
+public interface Logger {
+	void log(String message);
+}
